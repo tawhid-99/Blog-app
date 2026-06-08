5898d9b032298605f33477e58d3d53682b8ccd10
Tawhid <mohammed.tawhid@p99soft.com>
2026-06-08 15:25:57 +0530

    feat: add dotenv configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app.js";
+import { env } from "./config/env.js";
 
-app.listen(3000, () => {
-    console.log("server running on port 3000")
+app.listen(env.PORT, () => {
+    console.log(`Server running on PORT: ${env.PORT}`)
 })
